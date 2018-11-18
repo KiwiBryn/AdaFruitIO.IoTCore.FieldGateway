fc350231e7ba8cdbf4e64849ac2fd4a9424f5215
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2018-11-18 20:33:31 +1300

    Updated NuGet packages etc.

--- AdaFruitIO.Swagger/Properties/AssemblyInfo.cs
+++ AdaFruitIO.Swagger/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AdaFruitIO.Swagger")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("nSwag generated client for AdaFruit.IO")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("devMobile Software AdaFruit.IO client")]
 [assembly: AssemblyProduct("AdaFruitIO.Swagger")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
